2f77cac67fd2cf43aab84a8d2d101f3d54aab5ed
Doge <cooldogedev@gmail.com>
2024-06-03 08:52:19 +0300

    refactor(Opts): rearrange fields

--- util/opts.go
+++ util/opts.go
@@ -6,12 +6,12 @@ type Opts struct {
 	Addr string `yaml:"addr"`
 	// AutoLogin determines whether automatic login should be enabled.
 	AutoLogin bool `yaml:"auto_login"`
-	// Token is the authentication token that Spectrum uses to authenticate with servers.
-	// When making requests to servers, Spectrum sends this token to the server for validation.
-	Token string `yaml:"token"`
 	// LatencyInterval is the interval at which the latency of the connection is updated in milliseconds.
 	// The lower the interval, the more accurate the latency will be, but the more bandwidth it will use.
 	LatencyInterval int64 `yaml:"latency_interval"`
+	// Token is the authentication token that Spectrum uses to authenticate with servers.
+	// When making requests to servers, Spectrum sends this token to the server for validation.
+	Token string `yaml:"token"`
 }
 
 // DefaultOpts returns the default configuration options for Spectrum.
