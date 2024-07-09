2f4b3561215a6a10516589522b85ce74890001ae
Doge <cooldogedev@gmail.com>
2024-07-09 07:33:42 +0300

    feat(API): improved packet handling and code documentation

--- api/authentication.go
+++ api/authentication.go
@@ -1,17 +1,26 @@
 package api
 
+import "strings"
+
+// Authentication defines an interface for authentication methods.
 type Authentication interface {
+	// Authenticate checks if the provided token is valid.
 	Authenticate(token string) bool
 }
 
+// SecretBasedAuthentication implements the Authentication interface
+// using a secret token for authentication.
 type SecretBasedAuthentication struct {
 	secret string
 }
 
+// NewSecretBasedAuthentication creates a new SecretBasedAuthentication instance
+// with the given secret token.
 func NewSecretBasedAuthentication(secret string) *SecretBasedAuthentication {
 	return &SecretBasedAuthentication{secret: secret}
 }
 
+// Authenticate ...
 func (authentication *SecretBasedAuthentication) Authenticate(token string) bool {
-	return authentication.secret == token
+	return strings.EqualFold(authentication.secret, token)
 }
