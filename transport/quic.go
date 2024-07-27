8f5da74658887e9303e99e9661ca36a079cf7331
Doge <cooldogedev@gmail.com>
2024-07-27 06:43:31 +0300

    transport/quic.go: implemented connection timeout mechanism for graceful closure

--- transport/quic.go
+++ transport/quic.go
@@ -18,43 +18,62 @@ type session struct {
 	conn    quic.Connection
 	counter int32
 	mu      sync.Mutex
-}
-
-func createSession() *session {
-	s := &session{}
-	s.mu.Lock()
-	return s
+	timer   *time.Timer
 }
 
 func (s *session) openStream() (quic.Stream, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	if s.conn == nil {
+	select {
+	case <-s.conn.Context().Done():
 		return nil, net.ErrClosed
+	default:
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		stream, err := s.conn.OpenStreamSync(ctx)
+		if err != nil {
+			_ = s.conn.CloseWithError(0, "failed to open stream")
+			return nil, err
+		}
+
+		if s.timer != nil {
+			s.timer.Stop()
+			s.timer = nil
+		}
+
+		s.counter++
+		go func() {
+			<-stream.Context().Done()
+			s.closeStream()
+		}()
+		return stream, nil
+	}
+}
+
+func (s *session) closeStream() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.counter--
+	if s.counter > 0 {
+		return
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-
-	stream, err := s.conn.OpenStreamSync(ctx)
-	if err != nil {
-		_ = s.conn.CloseWithError(0, "failed to open stream")
-		return nil, err
+	if s.timer != nil {
+		s.timer.Reset(time.Second * 10)
+		return
 	}
-
-	s.counter++
-	go func() {
-		<-stream.Context().Done()
+	s.timer = time.AfterFunc(time.Second*10, func() {
 		s.mu.Lock()
 		defer s.mu.Unlock()
 
-		s.counter--
+		s.timer = nil
 		if s.counter <= 0 {
 			_ = s.conn.CloseWithError(0, "no open streams")
 		}
-	}()
-	return stream, nil
+	})
 }
 
 // QUIC implements the Transport interface to establish connections to servers using the QUIC protocol.
@@ -64,7 +83,7 @@ func (s *session) openStream() (quic.Stream, error) {
 type QUIC struct {
 	sessions map[string]*session
 	logger   *slog.Logger
-	mu       sync.RWMutex
+	mu       sync.Mutex
 }
 
 // NewQUIC creates a new QUIC transport instance.
@@ -77,20 +96,19 @@ func NewQUIC(logger *slog.Logger) *QUIC {
 
 // Dial ...
 func (q *QUIC) Dial(addr string) (io.ReadWriteCloser, error) {
-	if s := q.get(addr); s != nil {
-		stream, err := s.openStream()
-		if errors.Is(err, net.ErrClosed) {
-			q.logger.Debug("tried to open stream on a closed connection", "addr", addr)
-			return q.Dial(addr)
-		}
-		return stream, err
-	}
-
 	q.mu.Lock()
-	s := createSession()
-	q.sessions[addr] = s
-	q.mu.Unlock()
-	q.logger.Debug("created connection", "addr", addr)
+	defer q.mu.Unlock()
+
+	if s, ok := q.sessions[addr]; ok {
+		stream, err := s.openStream()
+		if err == nil {
+			return stream, nil
+		}
+
+		if !errors.Is(err, net.ErrClosed) {
+			return nil, err
+		}
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
@@ -114,18 +132,18 @@ func (q *QUIC) Dial(addr string) (io.ReadWriteCloser, error) {
 		},
 	)
 	if err != nil {
-		s.mu.Unlock()
-		q.remove(addr)
 		q.logger.Debug("failed to dial address", "addr", addr, "err", err)
 		return nil, err
 	}
 
-	s.conn = conn
-	s.mu.Unlock()
-	q.logger.Debug("successfully dialed address", "addr", addr)
+	s := &session{conn: conn}
+	q.sessions[addr] = s
+	q.logger.Debug("established connection", "addr", addr)
 	go func() {
 		<-conn.Context().Done()
-		q.remove(addr)
+		q.mu.Lock()
+		delete(q.sessions, addr)
+		q.mu.Unlock()
 		if err := conn.Context().Err(); err != nil && !errors.Is(err, context.Canceled) {
 			q.logger.Error("closed connection", "addr", addr, "err", err)
 		} else {
@@ -133,16 +151,4 @@ func (q *QUIC) Dial(addr string) (io.ReadWriteCloser, error) {
 		}
 	}()
 	return s.openStream()
-}
-
-func (q *QUIC) get(addr string) *session {
-	q.mu.RLock()
-	defer q.mu.RUnlock()
-	return q.sessions[addr]
-}
-
-func (q *QUIC) remove(addr string) {
-	q.mu.Lock()
-	defer q.mu.Unlock()
-	delete(q.sessions, addr)
 }
